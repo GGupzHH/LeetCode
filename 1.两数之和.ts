e49aa12fac7704f2579f611eb2705b9db9653b87
赵雅国 <zhao.yaguo@shinewingdigital.com>
2022-09-21 18:33:20 +0800

    feat: 01-两数之和

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -1,12 +1,17 @@
 /*
  * @lc app=leetcode.cn id=1 lang=typescript
- *
+ * 哈希表解法
  * [1] 两数之和
  */
 
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
-
+  const mapping = {}
+  for (let i = 0; i < nums.length; i++) { 
+    if (mapping[nums[i]] !== undefined) { 
+      return [mapping[nums[i]], i]
+    }
+    mapping[target - nums[i]] = i
+  }
 };
 // @lc code=end
-
